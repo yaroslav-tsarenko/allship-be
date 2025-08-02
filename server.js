a1b123eb192c3396f6a5d5f1d0ef3812c7f34802
Yaroslav <yaroslav7v@gmail.com>
2025-08-03 01:50:38 +0300

    new changes

--- server.js
+++ server.js
@@ -38,16 +38,22 @@ const allowedOrigins = [
   "https://www.dashboard.allship.ai"
 ];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  }
+
+  // Handle preflight requests
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 /*const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
